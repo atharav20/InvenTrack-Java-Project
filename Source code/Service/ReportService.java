7a0715df82de65566dc4372422ea53f35a40a2a8
Atharav Balaji Khonde <atharavkhonde06@gmail.com>
2026-09-15 20:48:12 +0530

    Add package and import statements to ReportService

--- Source code/Service/ReportService.java
+++ Source code/Service/ReportService.java
@@ -1,1 +1,8 @@
+package service;
+import model.Product;
+import util.InventoryException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
