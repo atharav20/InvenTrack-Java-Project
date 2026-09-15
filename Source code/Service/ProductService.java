34c2c1f3fe76e96dbf7e6340aef3bf1ab15b8b11
Atharav Balaji Khonde <atharavkhonde06@gmail.com>
2026-09-15 19:59:06 +0530

    Add package and import statements to ProductService

--- Source code/Service/ProductService.java
+++ Source code/Service/ProductService.java
@@ -1,1 +1,13 @@
+package service;
 
+import model.Product;
+import util.DBConnection;
+import util.InventoryException;
+import util.InventoryException.ErrorType;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
